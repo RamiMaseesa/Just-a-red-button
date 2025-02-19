217fe8b785d4a5dd02d30a8025870ca5cc8c13c0
Rafael Alcazar Paradas <ralcazarparadas01@student.rocvantwente.nl>
2025-02-19 20:02:56 +0100

    chore: ChangedPlayerFix

--- justaredbutton/Scripts/CheckObjectInView.gd
+++ justaredbutton/Scripts/CheckObjectInView.gd
@@ -8,5 +8,5 @@ func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 		object.get_parent_node_3d().visible = false;
 
 
-func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	buttonHasAppeared = true
